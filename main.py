ccaa6da400bb2d9218ccb97f2c65b31ff32ffcb1
kavyanataraj-dev <65570926+kavyanataraj-dev@users.noreply.github.com>
2020-05-19 12:34:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import requests
 from urllib.parse
 import urlparse
+import json
 
-def api_test()
- url = "https://api/subject/1594950620847472640/analysis_session"
+def test_post_headers_body_json():
+    url = 'https://clarity.dexcom.com/'
+    
+    # Additional headers.
+    headers = {'Content-Type': 'application/json' } 
 
- s = requests.Session()
-
- req = s.get(url)
-
- parsed = urlparse.urlparse(url)
- path = parsed[2]
-
- pathlist = path.split("/") #elements that comes after "https://api", here 's the elements of the list [' subject ', '1594950620847472640 ',
-analysis_session '] '
-
-
-assert pathlist[3] != NULL
-
+    # Request Body
+    payload = {'key1': 1, 'key2': 'value2'}
+    
+    # convert dict to json by json.dumps() for body data. 
+    resp = requests.post(url, headers=headers, data=json.dumps(payload,indent=4))       
+    
+    # Validate response headers and body contents, e.g. status code.
+    assert resp.status_code == 200
+    resp_body = resp.json()
+    assert resp_body['url'] == url
+    
+    # print response full body as text
+    print(resp.text)
